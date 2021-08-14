4af4d9270784cfdff2a0b756b7314dd425a73f11
Nguyen Ngoc Thuan <thuannnth2009019@fpt.edu.vn>
2021-08-14 14:03:54 +0700

    thuan

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.2 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function initMap() {
+    const uluru = {lat: 21.0326476, lng: 105.7723577};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 18,
+        center: uluru,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
